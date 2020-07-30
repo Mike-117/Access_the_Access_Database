8645c57dfbc21cec4899913ce757fd02df45a102
Mike-117 <61859863+Mike-117@users.noreply.github.com>
2020-07-30 11:37:51 -0600

    Add project files.

---
+++ ChooseDatabase.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.OleDb;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml;
+
+
+namespace Access_the_Access_Database
+{
+    /// <summary>
+    /// Interaction logic for ChooseDatabase.xaml
+    /// </summary>
+    public partial class ChooseDatabase : Page
+    {
+        public ChooseDatabase ()
+        {
+            InitializeComponent();
+        }
+
+       
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // Create OpenFileDialog
+            Microsoft.Win32.OpenFileDialog openFileDlg = new Microsoft.Win32.OpenFileDialog();
+
+            Nullable<bool> result = openFileDlg.ShowDialog();
+            if (result == true)
+            {
+
+                ShowDatabase showDatabase = new ShowDatabase();
+                this.NavigationService.Navigate(showDatabase);
+
+            }
+        }
+    }
+}
---
+++ ShowDatabase.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.OleDb;
+using System.Configuration;
+using System.Xml;
+
+namespace Access_the_Access_Database
+{
+    /// <summary>
+    /// Interaction logic for ShowDatabase.xaml
+    /// </summary>
+    public partial class ShowDatabase : Page
+    {
+        public ShowDatabase()
+        {
+            InitializeComponent();
+            loadlist();
+        }
+
+        private void loadlist()
+        {
+
+            OleDbConnection con = new OleDbConnection();
+            con.ConnectionString = ConfigurationManager.ConnectionStrings["Connection"].ToString();
+            con.Open();
+            OleDbCommand cmd = new OleDbCommand();
+            cmd.CommandText = "select ML_Name from [ML]";
+            cmd.Connection = con;
+            OleDbDataReader rd = cmd.ExecuteReader();
+            // DataSet ds = new DataSet();
+            listBox.ItemsSource = rd;
+
+        }
+    }
+}
+
